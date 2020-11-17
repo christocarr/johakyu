60af3ded28c18a010832c24f6b56c7eb86ddf828
christocarr <christocarr@outlook.com>
2020-11-17 16:42:38 +0000

    Chore: meta charset utf-8

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ function Layout({ children, title = 'Johakyu' }) {
 		<div className={styles.container}>
 			<Head>
 				<title>{title}</title>
+				<meta charSet="utf-8" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
 			<Header />
