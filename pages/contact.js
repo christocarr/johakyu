0c2a460b6f646e90bbb928b614edb4bc9de523dc
christocarr <christocarr@outlook.com>
2020-11-24 15:28:20 +0000

    Feature: update pages contents

--- pages/contact.js
+++ pages/contact.js
@@ -3,8 +3,26 @@ import styles from '../styles/Home.module.css';
 
 export default function Contact() {
 	return (
-		<main className="flex flex-col flex-grow p-5">
-			<h1>Contact Us</h1>
+		<main className="flex flex-col flex-grow p-5 pt-0">
+			<h1 className="font-semibold text-lg">Contact Us</h1>
+			<form className="p-5 mt-5 border border-gray-400 flex flex-col justify-center items-center">
+				<div className="w-5/6">
+					<label htmlFor="fName">First Name:</label>
+					<input className="w-full px-2 py-1 border border-gray-400" type="text" id="fName" name="fname" required />
+				</div>
+				<div className="w-5/6 mt-2">
+					<input className="w-full px-2 py-1 border border-gray-400" type="text" name="lname" placeholder="Last name"/>
+				</div>
+				<div className="w-5/6 mt-2">
+					<input className="w-full px-2 py-1 border border-gray-400" type="email" name="email" placeholder="Emai" required/>
+				</div>
+				<div className="w-5/6 mt-2">
+					<textarea className="w-full px-2 py-1 border border-gray-400" type="text" name="message" placeholder="Your message..." required></textarea>
+				</div>
+				<button className="w-5/6 mt-2 border px-2 py-1 border-gray-400 shadow font-semibold hover:shadow-none hover:bg-brand-red hover:text-white" type="button">Submit</button>
+			</form>
+			<div className="w-full h-auto mt-5 flex justify-around items-center">
+			</div>
 		</main>
 	);
 }
--- pages/projects.js
+++ pages/projects.js
@@ -3,8 +3,13 @@ import styles from '../styles/Home.module.css';
 
 export default function Projects() {
 	return (
-		<main className="flex flex-col flex-grow p-5">
-			<h1>Current Projects</h1>
+		<main className="flex flex-col flex-grow p-5 pt-0">
+			<h1 className="font-semibold text-lg">Current Projects</h1>
+			<p className="mt-5">We are currently working on several projects and will release details soon.</p>
+			<p className="mt-5">However, we at Jo-Ha-Kyū Arts are always interested in hearing about your own projects and ideas and will consider anything that falls in line with our company ethos.</p>
+			<p className="mt-5">We encourage you to get in contact via our online form. Looking forward to hearing from you!
+</p>
+			<p className="mt-5 italic">Jo-Ha-Kyū Arts Team</p>
 		</main>
 	);
 }
