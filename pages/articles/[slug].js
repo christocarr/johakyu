fdda7a21f79a62623cbc8dfee8e97e41eb238f85
christocarr <christocarr@outlook.com>
2020-11-26 14:15:28 +0000

    Feature: add blog functionality

---
+++ pages/articles/[slug].js
@@ -0,0 +1,56 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
+
+let client = require('contentful').createClient({
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN
+})
+
+export async function getStaticPaths() {
+
+  let data = await client.getEntries({
+    content_type: 'article'
+  })
+
+  return {
+    paths: data.items.map(item => ({
+      params: {slug: item.fields.slug}
+    })),
+    fallback: true
+  }
+}
+
+export async function getStaticProps({ params }) {
+  let data = await client.getEntries({
+    content_type: 'article',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: {
+      article: data.items[0]
+    }
+  }
+}
+
+
+export default function Article({ article }) {
+  console.log(article)
+  return (
+    <main className="flex flex-col flex-grow p-5 pt-0 sm:w-2/3 sm:mx-auto sm:mt-5">
+      <article>
+        <h1>{article.fields.title}</h1>
+        <div className="mt-2">
+          <Image 
+            width="800" 
+            height="500" 
+            className="object-cover object-center" 
+            src={'https:' + article.fields.articleImage.fields.file.url} alt={article.fields.articleImage.fields.title} 
+          />
+        </div>
+        <p className="mt-2">{documentToReactComponents(article.fields.content)}</p>
+      </article>
+    </main>
+
+  )
+}
