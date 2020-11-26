92626c5e845c8a1b0f3bf437ac26fc13c0a8c667
christocarr <christocarr@outlook.com>
2020-11-26 14:36:59 +0000

    Modify: contact form labels and input placeholder

--- pages/contact.js
+++ pages/contact.js
@@ -4,19 +4,21 @@ export default function Contact() {
 	return (
 		<main className="flex flex-col flex-grow p-5 pt-0 sm:w-2/3 sm:mx-auto sm:mt-5 lg:w-2/4">
 			<h1 className="font-semibold text-lg">Contact Us</h1>
-			<form className="p-5 mt-5 border border-gray-400 flex flex-col justify-center items-center lg:p-10">
+			<form className="p-5 mt-5 border border-gray-400 flex flex-col justify-center items-center shadow lg:p-10">
 				<div className="w-5/6">
-					<label htmlFor="fName">First Name:</label>
-					<input className="w-full px-2 py-1 border border-gray-400" type="text" id="fName" name="fname" required />
+					<label htmlFor="fName">Name<span className="text-brand-red">*</span></label>
+					<input className="w-full px-2 py-1 border border-gray-400" type="text" id="fName" name="fname" placeholder="First name" required />
 				</div>
 				<div className="w-5/6 mt-2">
 					<input className="w-full px-2 py-1 border border-gray-400" type="text" name="lname" placeholder="Last name"/>
 				</div>
 				<div className="w-5/6 mt-2">
-					<input className="w-full px-2 py-1 border border-gray-400" type="email" name="email" placeholder="Emai" required/>
+					<label htmlFor="email">Email<span className="text-brand-red">*</span></label>
+					<input className="w-full px-2 py-1 border border-gray-400" type="email" id="email" name="email" required/>
 				</div>
 				<div className="w-5/6 mt-2">
-					<textarea className="w-full px-2 py-1 border border-gray-400" type="text" name="message" placeholder="Your message..." required></textarea>
+					<label htmlFor="message">Message<span className="text-brand-red">*</span></label>
+					<textarea className="w-full px-2 py-1 border border-gray-400" type="text" id="message" name="message" placeholder="Your message..." required></textarea>
 				</div>
 				<button className="w-5/6 mt-2 border px-2 py-1 border-gray-400 shadow font-semibold hover:shadow-none hover:bg-brand-red hover:text-white lg:mt-5" type="button">Submit</button>
 			</form>
