c595fcf96720f3988fb67a2319e132602e568aa5
christocarr <christocarr@outlook.com>
2020-11-28 14:55:57 +0000

    Fix: image covering whole document

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
 	return (
 		<main className="flex flex-col flex-grow sm:w-2/3 sm:mx-auto sm:mt-5 lg:w-2/4 xl:w-2/5">
-			<div className="bg-red-brand-red w-full h-48 relative">
+			<div className="bg-red-brand-red w-full relative">
 				{/* <div className="bg-gray-900 bg-opacity-50 w-full h-full absolute"></div> */}
-				<Image layout="fill" className="object-cover object-center" src="/static/images/koto_edited.jpg" alt="person playing a koto" />
+				<img className="overflow-hidden" src="/static/images/koto_edited.jpg" alt="person playing a koto" />
 				<div className="absolute top-0 left-0 p-5 text-white font-semibold sm">
 					<p>Genre-defying</p>
 					<p>Innovation <span className="text-brand-red">meets</span> Tradition <span className="text-brand-red">meets</span> Diversity</p>
