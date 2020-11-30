f0dc26471309cfdfe8b2cc6b0c887d7fdf42112d
christocarr <christocarr@outlook.com>
2020-11-30 17:21:51 +0000

    Style: form background color opacity button margin

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
 	return (
 		<main className="font-roboto flex flex-col flex-grow p-5 pt-0 sm:w-2/3 sm:mx-auto sm:mt-5 lg:w-2/4 xl:w-2/5">
 			<h1 className="font-semibold text-lg font-jura">Contact Us</h1>
-			<form className="p-5 mt-5 border border-gray-400 flex flex-col justify-center items-center shadow lg:p-10">
+			<form className="p-5 mt-5 border bg-brand-red bg-opacity-20 border-gray-400 flex flex-col justify-center items-center shadow lg:p-10">
 				<div className="w-5/6">
 					<label htmlFor="fName">Name<span className="text-brand-red">*</span></label>
 					<input className="w-full px-2 py-1 border border-gray-400" type="text" id="fName" name="fname" placeholder="First name" required />
@@ -20,7 +20,7 @@ export default function Contact() {
 					<label htmlFor="message">Message<span className="text-brand-red">*</span></label>
 					<textarea className="w-full px-2 py-1 border border-gray-400" type="text" id="message" name="message" placeholder="Your message..." required></textarea>
 				</div>
-				<button className="w-5/6 mt-2 border px-2 py-1 border-gray-400 shadow font-semibold hover:shadow-none hover:bg-brand-red hover:text-white lg:mt-5" type="button">Submit</button>
+				<button className="w-5/6 my-2 border px-2 py-1 bg-white border-gray-400 shadow font-semibold hover:shadow-none hover:bg-brand-red hover:text-white lg:mt-5" type="button">Submit</button>
 			</form>
 			<div className="w-full h-10 mt-10 flex justify-around items-center">
 				<Link href="" >
