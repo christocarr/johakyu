fc404844732a686ac5e964cbec6f25d76c692ce0
christocarr <christocarr@outlook.com>
2020-12-02 16:11:14 +0000

    Modify: footer copyright notice

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
 	return (
 		<footer className="p-5 mt-5 border-t h-20 flex justify-center items-center lg:p-10">
-			<p className="font-roboto">Copyright 2020</p>
+			<p className="font-roboto text-sm text-gray-800">Copyright &#169; 2020 Jo-Ha-Kyū Arts ltd. All rights reserved.</p>
 		</footer>
 	);
 }
