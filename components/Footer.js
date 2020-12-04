61b1034a41882996831692d546a758e13ddd2742
christocarr <christocarr@outlook.com>
2020-12-04 12:25:50 +0000

    Style: remove margin

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
 	return (
-		<footer className="p-5 mt-5 border-t h-20 flex justify-center items-center lg:p-10">
+		<footer className="p-5 border-t h-20 flex justify-center items-center lg:p-10">
 			<p className="font-roboto text-sm text-gray-800">Copyright &#169; 2020 Jo-Ha-Kyū Arts ltd. All rights reserved.</p>
 		</footer>
 	);
