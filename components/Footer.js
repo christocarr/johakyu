c197820d971487da64873e34f36986395f66762d
christocarr <christocarr@outlook.com>
2020-12-08 19:35:09 +0000

    Style: change wording and layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,8 @@
 function Footer() {
 	return (
-		<footer className="p-5 border-t h-20 flex justify-center items-center lg:p-10">
-			<p className="font-roboto text-sm text-gray-800">Copyright &#169; 2020 Jo-Ha-Kyū Arts ltd. All rights reserved.</p>
+		<footer className="p-5 border-t h-20 flex flex-col justify-center items-center lg:p-10">
+			<p className="font-roboto text-sm text-gray-800">Copyright &#169; 2020 Jo-Ha-Kyū Arts Limited.</p>
+			<p className="font-roboto text-sm text-gray-800">All rights reserved.</p>
 		</footer>
 	);
 }
